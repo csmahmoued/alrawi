f265e54057bc60513e6da648fa450a44ac2e5fa8
csmahmoued <cs.mahmoued@gmail.com>
2025-12-12 22:30:01 +0200

    init

---
+++ src/main/java/eg/alrawi/alrawi_award/AlrawiAwardApplication.java
@@ -0,0 +1,13 @@
+package eg.alrawi.alrawi_award;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlrawiAwardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlrawiAwardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eg/alrawi/alrawi_award/AlrawiAwardApplicationTests.java
@@ -0,0 +1,13 @@
+package eg.alrawi.alrawi_award;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlrawiAwardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
